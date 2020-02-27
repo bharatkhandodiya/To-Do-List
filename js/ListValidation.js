b5555527ad51d74cde64a455385de43888a8ac4b
bharat <bharatkhandodiya@gmail.com>
2020-02-27 16:03:29 +0530

    Finish Button remove  because it not working

--- js/ListValidation.js
+++ js/ListValidation.js
@@ -11,15 +11,15 @@ for(i=0;i<close_list.length;i++){
 
 // Create a "checked" button and append it to each list item
 
-var check_list = document.getElementsByTagName("li");
-var i;
-for(i=0;i<check_list.length;i++){
-  var checked = document.createElement("button");
-  var checked_Text = document.createTextNode("Finish");
-  checked.className ="checked";
-  checked.appendChild(checked_Text);
-  check_list[i].appendChild(checked);
-}
+// var check_list = document.getElementsByTagName("li");
+// var i;
+// for(i=0;i<check_list.length;i++){
+//   var checked = document.createElement("button");
+//   var checked_Text = document.createTextNode("Finish");
+//   checked.className ="checked";
+//   checked.appendChild(checked_Text);
+//   check_list[i].appendChild(checked);
+// }
 
 // Click on a close button to hide the current list item
 
@@ -33,12 +33,12 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('checked');
-console.log(list);
-list.addEventListener('click', function() {
-   list.style.textDecoration="line-through";
-   console.log(list)
-});
+// var list = document.querySelector('checked');
+// console.log(list);
+// list.addEventListener('click', function() {
+//    list.style.textDecoration="line-through";
+//    console.log(list)
+// });
 
 // Add function
 
@@ -63,11 +63,11 @@ function add(){
   create.appendChild(t);
   li.appendChild(create)
   
-  var checked = document.createElement("button");
-  var checked_Text = document.createTextNode("Finish");
-  checked.className ="checked";
-  checked.appendChild(checked_Text);
-  li.appendChild(checked);
+  // var checked = document.createElement("button");
+  // var checked_Text = document.createTextNode("Finish");
+  // checked.className ="checked";
+  // checked.appendChild(checked_Text);
+  // li.appendChild(checked);
   
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
